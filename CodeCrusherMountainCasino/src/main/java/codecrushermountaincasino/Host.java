4efcf0a3f850ed7e373c145df1ae17584d678be0
joseph <joseph@Zipcoder06s-MacBook-Pro.local>
2016-01-30 12:40:50 -0500

    Add stubbed Host Class

--- CodeCrusherMountainCasino/src/main/java/codecrushermountaincasino/Host.java
+++ CodeCrusherMountainCasino/src/main/java/codecrushermountaincasino/Host.java
@@ -4,4 +4,27 @@ package codecrushermountaincasino;
  * Created by Taylor on 1/30/16.
  */
 public class Host {
+    public Host() {
+
+    }
+
+    public void greet() {
+
+    }
+
+    public String askName() {
+        return null;
+    }
+
+    public int askWhatGame() {
+        return 0;
+    }
+
+    public void giveChips() {
+
+    }
+
+    public void tellJoke() {
+        
+    }
 }
